5a1a6185f042e094f3da3c3a1f2a2960dad45995
Joel <fitness.joeld@gmail.com>
2024-08-23 14:14:24 -0400

    lesson 13 Footer fixed

--- js/index.js
+++ js/index.js
@@ -1,16 +1,21 @@
-const footerChild = document.createElement('footer');
-document.body.appendChild(footerChild);
+const footer = document.createElement('footer');
+document.body.appendChild(footer);
 
 let today = new Date();
+
 let thisYear = today.getFullYear();
 
-const footer = document.querySelector('footer');
+
 const copyright = document.createElement('p');
+
 copyright.innerHTML = `Joel DeLeon ${thisYear}`;
-document.body.appendChild(copyright);
+
+footer.appendChild(copyright);
 
 let skills = ["JavaScript", "HTML", "CSS", "Adobe Photoshop", "GitHub"];
+
 let skillsSection = document.querySelector("#skills");
+
 let skillsList = skillsSection.querySelector('ul');
 
 for (let i = 0; i < skills.length; i++) {
